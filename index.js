65c610a8ef8ebef5cd6460fe44435272e09ac4d5
Francis Maina <sebastianfrancix@gmail.com>
2022-08-31 02:10:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    const complementaryNumber = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === complementaryNumber) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
 */
-
+// O(N^2)
 /* 
   Add your pseudocode here
 */
-
-/*
-  Add written explanation of your solution here
-*/
+// Iterate over the array of numbers, for the current number, find a complementary number that, when added together, equals our target number (for example: if our number is 3, and the target is 10, the complementary number is 7)
+// Iterate over the remaining numbers in the array to see if the complementary number is present; if so, returning true
+// If we reach the end of the array without finding a complementary number,  return false.
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
